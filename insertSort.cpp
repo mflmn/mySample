8b75626a8a9123aa556f86b0cac2449925ec19ea
anson <anson@ansondeMacBook-Pro.local>
2018-03-11 17:34:13 +0800

    modify insert sort

--- insertSort.cpp
+++ insertSort.cpp
@@ -2,6 +2,26 @@
 
 using namespace std;
 
+void insertSort(int a[])
+{
+    for (int i = 1; i < 7; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            if (a[j] < a[j - 1])
+            {
+                int temp = a[j - 1];
+                a[j - 1] = a[j];
+                a[j] = temp;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
+
 int main(int argc, char** argv)
 {
     int a[7] = {2,4,1,3,7,9,5};
